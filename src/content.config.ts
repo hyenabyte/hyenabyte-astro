629075a0965913275c3219c40b21524c9d76757d
hyena <hyena@hyenabyte.dev>
2025-03-06 13:36:24 +0100

    chore: wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,9 +16,34 @@ export type BlogPost = {
   collection: 'blog';
   data: z.infer<typeof blogPostSchema>;
 };
+
 const blog = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/posts' }),
   schema: blogPostSchema,
 });
 
-export const collections = { blog };
+const portfolioItemSchema = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      thumbnail: image(),
+      url: z.string().optional(),
+      tags: z.string().array().optional(),
+    }),
+});
+
+export type PortfolioItem = {
+  id: string;
+  slug: string;
+  body: string;
+  collection: 'portfolio';
+  data: z.infer<typeof portfolioItemSchema>;
+};
+
+const portfolio = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/portfolio' }),
+  schema: portfolioItemSchema,
+});
+
+export const collections = { blog, portfolio };
---
+++ src/content/portfolio/onki-tv.md
@@ -0,0 +1,12 @@
+---
+title: 'Onki.tv'
+description: 'Lorem ipsum dolor sit amet'
+url: 'https://vfx.onki.tv'
+tags:
+  - vue
+  - nuxt
+  - frontend
+  - design
+---
+
+# Onki.tv
